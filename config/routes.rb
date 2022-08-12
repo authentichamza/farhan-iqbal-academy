1d854bc303d1b4e024969645bba4b77d5c604dd0
authentichamza <hamzashams446@gmail.com>
2022-08-12 20:10:55 +0500

    Template Ready

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :homes, only: :index
+  resources :abouts, only: :index
+  resources :courses, only: :index
   resources :contacts, only: :create
-  root to: 'contacts#index'
+
+  root to: 'homes#index'
 end
